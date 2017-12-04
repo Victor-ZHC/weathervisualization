d94b49281636688aa58e3fddccd14e65912f715c
Victor <1341634255@qq.com>
2017-12-04 13:25:48 +0800

    中心城区崇明县面积人口修改

--- src/main/java/com/adc/disasterforecast/global/FeiteRegionInfo.java
+++ src/main/java/com/adc/disasterforecast/global/FeiteRegionInfo.java
@@ -3,22 +3,22 @@ package com.adc.disasterforecast.global;
 public class FeiteRegionInfo {
 
     // region info
-    public static final String XH_DISTRICT = "13";
+//    public static final String XH_DISTRICT = "13";
     public static final String XH_AREA = "徐汇区";
-    public static final Double XH_POPULATION = 108.56;
-    public static final Double XH_ACREAGE = 54.93;
+//    public static final Double XH_POPULATION = 108.56;
+//    public static final Double XH_ACREAGE = 54.93;
     public static final String XH_STATION_NAME = "徐家汇";
 
     public static final String CM_DISTRICT = "10";
     public static final String CM_AREA = "崇明区";
-    public static final Double CM_POPULATION = 67.26;
-    public static final Double CM_ACREAGE = 1411.0;
+    public static final Double CM_POPULATION = 69.72;
+    public static final Double CM_ACREAGE = 1185.49;
     public static final String CM_STATION_NAME = "崇明";
 
     public static final String[] CENTRAL_DISTRICT = {"11", "12", "13", "14", "15", "16", "17", "18"};
     public static final String CENTRAL_AREA = "中心城区";
-    public static final Double CENTRAL_POPULATION = 1000.0;
-    public static final Double CENTRAL_ACREAGE = 660.0;
+    public static final Double CENTRAL_POPULATION = 707.88;
+    public static final Double CENTRAL_ACREAGE = 289.44;
 
     public static final int Hours = 41;
     public static final int Top10 = 10;
