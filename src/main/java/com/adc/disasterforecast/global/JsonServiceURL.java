aa84feb3dedc88ef7d394850659aec0d82865a93
wangtianxiong <wangtianxiong@hotmail.com>
2018-04-25 16:42:25 +0800

    替换ip

--- src/main/java/com/adc/disasterforecast/global/JsonServiceURL.java
+++ src/main/java/com/adc/disasterforecast/global/JsonServiceURL.java
@@ -2,14 +2,14 @@ package com.adc.disasterforecast.global;
 
 public class JsonServiceURL {
 
-    public static final String ALARM_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/AlarmJsonService.svc/";
-    public static final String AUTO_STATION_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/AutoStationJsonService.svc/";
-    public static final String FORECAST_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/ForecastJsonService.svc/";
-    public static final String METEOROLOGICAL_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/MeteorologicalJsonService.svc/";
-    public static final String PUMP_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/PumpJsonService.svc/";
-    public static final String TYPHOON_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/TyphoonJsonService.svc/";
-    public static final String THUNDER_JSON_SERVICE_URL = "http://61.152.126.152/JsonService_V2/ThunderJsonService.svc/";
-    public static final String VERIFY_USER_URL = "http://61.152.126.152/JsonService_V2/UserJsonService.svc/";
+    public static final String ALARM_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/AlarmJsonService.svc/";
+    public static final String AUTO_STATION_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/AutoStationJsonService.svc/";
+    public static final String FORECAST_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/ForecastJsonService.svc/";
+    public static final String METEOROLOGICAL_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/MeteorologicalJsonService.svc/";
+    public static final String PUMP_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/PumpJsonService.svc/";
+    public static final String TYPHOON_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/TyphoonJsonService.svc/";
+    public static final String THUNDER_JSON_SERVICE_URL = "http://116.239.25.38/JsonService_V2/ThunderJsonService.svc/";
+    public static final String VERIFY_USER_URL = "http://116.239.25.38/JsonService_V2/UserJsonService.svc/";
 
     public static final String HEALTH_SEND_NUM_URL = "http://222.66.83.21:8282/V2/patrolhandler.do/?provider=MMShareBLL.DAL.HeathWeatheReport&method=GetSendNum&Date=";
 
